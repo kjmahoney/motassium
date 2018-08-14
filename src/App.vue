7461e1539685fbf9fe9905735b14a891038669fe
Kevin Mahoney <kmahoney@theatlantic.com>
2018-08-14 17:12:17 -0400

    initial commit

---
+++ src/App.vue
@@ -0,0 +1,53 @@
+<template>
+  <div id="app">
+    <img src="./assets/logo.png">
+    <HelloWorld/>
+    <Total/>
+    <div class="firebase-data">
+      <div v-for="item in items">
+       <p><a v-bind:href="item.coffee">{{item.coffee}}</a></p>
+     </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import Firebase from 'firebase'
+
+import HelloWorld from './components/HelloWorld'
+import Total from './components/total'
+
+const config = {
+  apiKey: 'AIzaSyDBL7T8sQFweg5_8kJRIR1xW8wzKUhqej4',
+  authDomain: 'motassium.firebaseapp.com',
+  databaseURL: 'https://motassium.firebaseio.com',
+  storageBucket: 'motassium.appspot.com',
+  messagingSenderId: '787203548830',
+};
+
+const app = Firebase.initializeApp(config)
+const db = app.database()
+const itemsRef = db.ref('motassium')
+
+export default {
+  name: 'App',
+  components: {
+    HelloWorld,
+    Total
+  },
+  firebase: {
+    items: itemsRef
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ src/components/total.vue
@@ -0,0 +1,19 @@
+<template lang="html">
+  <div class="c-total">
+    {{ msg }}
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Total',
+  data() {
+    return {
+      msg: 'Firebase data goes here',
+    }
+  }
+}
+</script>
+
+<style lang="css">
+</style>
